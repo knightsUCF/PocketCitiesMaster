f73ed7b25f6ecd60f21d693ee26bb997dfc00d8d
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 18:36:28 -0500

    Create initialize.py

---
+++ app/initialize.py
@@ -0,0 +1,10 @@
+''' initialize.py '''
+import window
+StartingWindow = window.window()
+
+class initialize():
+	def __init__(self):
+		''' game initialization state variable data '''
+
+	def Start(self):
+		StartWindow.InitializeWindow()
