fbc5ff419f1051b5fc003480953c30d4e664f8c1
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 18:51:21 -0500

    Update state.py

--- app/state.py
+++ app/state.py
@@ -1,8 +1,36 @@
 ''' state.py '''
 
 class state():
-	def __init__(self):
+	def __init__(self, PlayerName,
+		           CityName,
+		           PlayerCharacter, 
+		           PlayerCharacters,
+		           StartingCapital, 
+		           Capital,
+		           Score,
+		           Mana,
+		           Points,
+		           Gold,
+		           Health,
+		           Map,
+		           TileInventor
+		    ):
+		
 		''' initialize state data '''
+		self.PlayerName = PlayerName
+		self.CityName = CityName
+		self.PlayerCharacter = PlayerCharacter
+		self.PlayerCharacters = PlayerCharacters
+		self.StartingCapital = StartingCapital
+		self.Capital = Capital
+		self.Score = Score
+		self.Mana = Mana
+		self.Points = Points
+		self.Gold = Gold
+		self.Health = Health
+		self.Map = Map
+		self.TileInventory = TileInventory
+		
 		return
 	
 	def UpdateState(self):
