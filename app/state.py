c298426b26622539d83f98037dab88bd7aa9469d
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 18:44:34 -0500

    Update state.py

--- app/state.py
+++ app/state.py
@@ -4,6 +4,11 @@ class state():
 	def __init__(self):
 		''' initialize state data '''
 		return
+	
+	def UpdateState(self):
+		return
+	
 	def Run(self):
 		''' update the state '''
+		self.UpdateState()
 		return
