144f64fd1e1de3077050e80cc2eab5110e8bb6ec
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 18:37:24 -0500

    Create render.py

---
+++ app/render.py
@@ -0,0 +1,22 @@
+''' render.py '''
+
+import state
+GameState = state.state()
+
+class render():
+	def __init__(self):
+		''' initialize state '''
+		return 
+
+	def Run(self):
+		UpdateRender()
+
+	def DisplayInitialState(self):
+		''' run this outside of game loop 
+		    example: GameRender.Initialize
+		'''
+		return 
+
+	def UpdateRender():
+		StateInfo.Run()
+		DrawState.Run()
