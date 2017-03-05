57fb10785eb3a7e971a1c1a29ae2fd676a7decd7
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 18:37:07 -0500

    Create engine.py

---
+++ app/engine.py
@@ -0,0 +1,34 @@
+''' engine.py '''
+
+import log
+io = log.log()
+
+
+class engine():
+
+	def GameLoop(self):
+		running = True
+		
+		while running:
+
+			io.o('Walking down the sidewalk... >>> >>> the main game loop >>> >>>')
+			''' time stamp '''
+			GameRender.Run()
+			GameInput.Run()
+			GameState.Run()
+			GameSync.Run()
+
+		    for event in pygame.event.get():
+		    	self.QuitGameRoutine()
+		    	running = False
+		    	pygame.quit()
+
+	def QuitGameRoutine(self):
+		''' Clean up 
+            A) Ask player if they are sure they want to quit?
+             
+		'''
+		return
+
+	def Run(self):
+		self.GameLoop()
