39cd5dfdff40d9835e60cd1befc3a90313ab5954
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 22:46:58 -0500

    Create gamemenu.py

---
+++ app/gamemenu.py
@@ -0,0 +1,82 @@
+class gamemenu():
+    def __init__(self):
+        return
+        
+        
+    def OnRevealMenu(self):
+        return
+        
+    def OnCloseMenu(self):
+        return 
+        
+        
+        
+    def OnBuild(self):
+        return 
+    
+    def OnBuildChoice(self):
+        return 
+        
+        
+        
+    def OnCreateNewCharacter(self):
+        return
+        
+    def OnNameNewCharacter(self):
+        return
+        
+    def OnEquipCharacter(self):
+        return
+        
+    def OnAssignProfessionToCharacter(self):
+        return
+        
+    def OnKillCharacter(self):
+        return 
+        
+    def OnBribeCharacter(self):
+        return 
+        
+        
+    
+    def OnCreateNewCity(self):
+        return
+        
+    def OnSelectDevelopNewResearch(self):
+        return 
+        
+    def OnSelectIncreaseEducation(self):
+        return 
+        
+
+    def OnViewInventory(self):
+        return
+        
+    def OnAddToInventory(self):
+        return
+        
+    def OnRemoveFromInventory(self):
+        return 
+        
+    
+    
+    def OnInitiateTrade(self):
+        return
+        
+    def OnCompleteTrade(self):
+        return 
+        
+    
+    
+    def OnRobOtherPlayer(self):
+        return
+        
+    
+        
+    def OnExitToMainInGameMenu(self):
+        return 
+        
+    def OnExitToMainOutGameMenu(self):
+        return 
+        
+   
