abb214156f298e34952807daf1d878f77d6f1822
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:44:18 -0500

    Update tile.py

--- app/tile.py
+++ app/tile.py
@@ -4,6 +4,11 @@ TileState = state.state()
 
 
 class tile():
+    
+    def __init__(self, type, ID, position):
+        self.type = type
+        self.ID = ID
+        self.position = position
 
     def CreateTileDatabase(self):
         return 
