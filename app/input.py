710f44a4ffc76b5f189a90ac1ee2b228f2d9e9ac
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:16:46 -0500

    Update input.py

--- app/input.py
+++ app/input.py
@@ -1,6 +1,16 @@
-'' input.py '''
+''' input.py '''
 
 class input():
 
 	def GetInput(self):
 		return
+		
+		
+	''' GOAL: Get user input and let them select a tile on the screen 
+	           
+		  A) Create a tile palette they can choose tiles from
+		  B) Let them move the box selection indicator with the up and down arrows
+		  C) Let them select -- indicate selection with fx - graphics and sound
+	
+	
+	'''
