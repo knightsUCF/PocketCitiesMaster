3f20a325e054d665e3ad1e3a7aa45e55ef62c75d
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:21:28 -0500

    Create tile.py

---
+++ app/tile.py
@@ -0,0 +1,18 @@
+
+
+class tile():
+
+    def UpdateTileMap():
+        return 
+        
+    def UpdateTileState(self):
+        self.UpdateTileMap()
+        return 
+    
+    def Run(self):
+        self.UpdateTileState()
+        return
+        
+        
+        
+    
