6d41725100ae767619a9ee838cbb5a609b6cca4e
peterknights1 <peterk@knights.ucf.edu>
2017-03-06 01:32:07 -0500

    Update chat.py

--- app/chat.py
+++ app/chat.py
@@ -2,14 +2,14 @@ class chat():
     def __init__(self):
         return
         
+    # run this outside of the game loop
     def InitializeChat(self):
         return 
-        
+    # run this inside of the game loop
     def UpdateChat(self):
         return
         
     def Run(self):
-        self.InitializeChat()
         self.UpdateChat()
         
         
