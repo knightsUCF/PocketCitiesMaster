e229f9b5a421e80dea8a0d84b1db146725aab70d
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:24:02 -0500

    Update tile.py

--- app/tile.py
+++ app/tile.py
@@ -1,16 +1,30 @@
+import state
+TileState = state.state()
+
 
 
 class tile():
 
     def UpdateTileMap():
+        TileState.GetTileTypeAndPosition()
         return 
         
     def UpdateTileState(self):
         self.UpdateTileMap()
         return 
     
+    def DrawTiles(self):
+        return
+    
+    def TileLibrary(self):
+        return
+    
+    def GenerateStartingTiles(self):
+        return 
+    
     def Run(self):
         self.UpdateTileState()
+        self.DrawTiles()
         return
         
         
