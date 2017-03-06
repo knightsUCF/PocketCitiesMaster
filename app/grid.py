1a199c2f24f0036f81e56aa5d9cefee9b68eb214
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 21:42:46 -0500

    Update grid.py

--- app/grid.py
+++ app/grid.py
@@ -1,6 +1,17 @@
 class grid():
-    def __init__(self, StartingGridSize):
+    def __init__(self, StartingGridSize, VerticalNumberOfLines, HorizontalNumberOfLines):
         self.StartingGridSize = StartingGridSize
+        self.VerticalNumberOfLines = VerticalNumberOfLines
+        self.HorizontalNumberOfLines = HorizontalNumberOfLines
+        return
+    
+    def DrawGrid(self):
+        return
+        
+    def StartDrawingRow(self):
+        return
+        
+    def StartDrawingColumn(self):
         return 
         
       
