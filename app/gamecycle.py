6b3c69050d64e28a089d5867b6d10744b2796c75
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:06:34 -0500

    Create gamecycle.py

---
+++ app/gamecycle.py
@@ -0,0 +1,11 @@
+class gamecycle():
+    def __init__(self):
+        ''' initialize game cycle '''
+        return 
+        
+    def Update(self):
+        return 
+        
+    def Run(self):
+        self.Update()
+        return
