801a3b9d1520f82d1ac35eb198e9db227ec69a62
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:32:02 -0500

    Create audioplayer.py

---
+++ app/audioplayer.py
@@ -0,0 +1,10 @@
+class audioplayer():
+    def __init__(self):
+        return 
+    
+    def UpdateCurrentlyPlayingSong(self):
+        return 
+    
+    def Run(self):
+        self.UpdateCurrentlyPlayingSong
+        
