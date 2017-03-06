a5d7ca45910c768fa9c5068da948c2cdaf780359
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:56:24 -0500

    Create map.py

---
+++ app/map.py
@@ -0,0 +1,15 @@
+
+
+
+class map():
+    def __init__(self):
+        ''' initialize map state to beginning of game '''
+        return 
+        
+    def Update(self): 
+        return 
+        
+    def Run(self):
+        self.Update()
+        
+        
