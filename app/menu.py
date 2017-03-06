e9905c8ea1d28e1dc28410f079cd840b8c318772
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:57:31 -0500

    Create menu.py

---
+++ app/menu.py
@@ -0,0 +1,14 @@
+
+
+class menu():
+    def __init__(self):
+        ''' Beginning menu state '''
+        return 
+        
+    def Update(self):
+        return 
+        
+    def Run(self):
+        self.Update
+        
+        
