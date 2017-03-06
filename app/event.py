004b3b8fe49e5a69e20d62f13c44929ca27fdf4d
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:11:15 -0500

    Update event.py

--- app/event.py
+++ app/event.py
@@ -13,6 +13,27 @@ class event():
     def OnWinGame(self):
         ''' Show You Won Congratulations screen '''
         return 
+    
+    def OnCreateNewCharacter(self):
+        return 
+    
+    def OnKillCharacter(self):
+        return
+    
+    def OnTradeCharacter(self):
+        return 
+    
+    def OnBuyNewCharacter(self):
+        return 
+    
+    def OnBirthNewCharacter(self):
+        return 
+    
+    def OnPopulationGrowth(self):
+        return
+    
+    def OnBankruptcy(self):
+        return 
         
     def Run(self):
         self.OnTileSelect()
