bc6ae9ca0110bc730018a8df266072b5255153fc
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:41:16 -0500

    Update sync.py

--- app/sync.py
+++ app/sync.py
@@ -1,9 +1,16 @@
 ''' sync.py '''
+import time 
+
 
 class sync():
 	def __init__(self):
 		return
-
+	
+	def GoToSleepForALittleWhile(self):
+		time.sleep(1)
+	
 	def Run(self):
 		''' Measure time lapse ''' 
+		self.GoToSleepForALittleWhile()
 		return
+	
