cb966d534c906394d1635caee0b00d5853edb819
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 23:06:54 -0500

    Create clan.py

---
+++ app/clan.py
@@ -0,0 +1,17 @@
+class clan():
+    def __init__(self, ID):
+        self.ID = ID
+        
+    def Clear(self, ID):
+        self.Alliance = false 
+        return
+        
+    def CreateNewClan(self, ID):
+        self.Alliance = true 
+        return
+        
+    def ClanPerks(self, ID):
+        return 
+        
+        
+    
