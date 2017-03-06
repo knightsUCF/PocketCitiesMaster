32c61f9c2f13f09751f95835e7a6b24594b0d096
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:27:47 -0500

    Update character.py

--- app/character.py
+++ app/character.py
@@ -1,10 +1,11 @@
 class character():
-    def __init__(self, CharacterID, Name, Type, Perk, Penalty):
+    def __init__(self, CharacterID, Name, Type, Perk, Penalty, Affiliation):
         self.CharacterID = CharacterID
         self.Name = Name
         self.Type = Type
         self.Perk = Perk
         self.Penalty = Penalty 
+        self.Affiliation = Affiliation
         ''' initialize character state data '''
         return 
         
