1a5b5c21bfbfa2ae92c36ec1f31007446756cac0
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:21:04 -0500

    Create weapons.py

---
+++ app/weapons.py
@@ -0,0 +1,15 @@
+class weapons():
+    def __init__(self, Type, ID, Cost, DamageXP):
+        ''' initialize weapon information '''
+        self.Type = Type
+        self.ID = ID
+        self.Cost = Cost
+        self.DamageXP = DamageXP
+        
+    def AddWeaponToInventory(self):
+        return 
+        
+    def DeleteWeaponFromInventory(self):
+        return 
+        
+    
