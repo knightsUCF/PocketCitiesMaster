c6f9bab71e35840e7b33b03429bfa448cebf3d59
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:23:45 -0500

    Create inventory.py

---
+++ app/inventory.py
@@ -0,0 +1,26 @@
+class inventory():
+    def __init__(self, StartingInventory, WeaponInventory, GadgetInventory):
+        self.StartingInventory = StartingInventory
+        self.WeaponInventory = WeaponInventory
+        self.GadgetInventory = GadgetInventory
+        
+    def UpdateInventory(self):
+        return
+        
+    def AddItemToInventory(self, ID):
+        return 
+        
+    def DeleteItemFromInventory(self, ID):
+        return 
+        
+    def UpgradeItemInInventory(self, ID):
+        return 
+        
+    def SellItemInInventory(self, ID):
+        return 
+        
+    def TradeItemInInventory(self, ID):
+        return 
+        
+        
+        
