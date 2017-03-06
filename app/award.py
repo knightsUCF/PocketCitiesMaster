0673989c4b4be17207397a07d208e60ffb3c3b8d
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 22:32:26 -0500

    Create award.py

---
+++ app/award.py
@@ -0,0 +1,21 @@
+class award():
+    def __init__(self, ID):
+        self.ID = ID
+        
+    def TypesOfAwards(self):
+        self.Noble
+        self.Courageous
+        self.Aristocrat
+        
+        self.Epic
+        self.Legendary
+        self.Mythical
+        self.Transcedent 
+        
+        
+        
+    def GiveAward(self):
+        return
+        
+    
+    
