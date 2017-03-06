c25151f8084d4ad68e7d75dad435c2e4a9fef24b
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:48:55 -0500

    Create tilemgr.py

---
+++ app/tilemgr.py
@@ -0,0 +1,19 @@
+import tile
+
+
+class tilemgr():
+
+    def __init__(self):
+        ''' initial state of tile manager '''
+        return 
+        
+    def Arrange(self):
+        return 
+        
+    def Update(self):
+        return 
+        
+    def Run(self):
+        self.Update()
+        
+        
