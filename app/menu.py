f53733b7791549919fd069a491190b7119e2a9b2
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 19:59:53 -0500

    Update menu.py

--- app/menu.py
+++ app/menu.py
@@ -1,8 +1,18 @@
 
 
 class menu():
-    def __init__(self):
+    def __init__(self, GameType, NewGame, StartGame, SaveGame, Exit, CurrentSelection):
         ''' Beginning menu state '''
+        
+        self.GameType = GameType
+        
+        self.NewGame = NewGame
+        self.StartGame = StartGame
+        self.SaveGame= SaveGame
+        self.Exit = Exit
+        
+        self.CurrentSelection = CurrentSelection
+        
         return 
         
     def Update(self):
