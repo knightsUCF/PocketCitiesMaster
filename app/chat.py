cd06d3d809aa6a19154962bbfb847c4835718b85
peterknights1 <peterk@knights.ucf.edu>
2017-03-06 01:32:30 -0500

    Update chat.py

--- app/chat.py
+++ app/chat.py
@@ -2,7 +2,7 @@ class chat():
     def __init__(self):
         return
         
-    # run this outside of the game loop
+    # run this first and once outside of the game loop
     def InitializeChat(self):
         return 
     # run this inside of the game loop
