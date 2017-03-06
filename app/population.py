f55fb71cd1a9d57dd0e6b55fb818630ea1823f31
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:08:00 -0500

    Create population.py

---
+++ app/population.py
@@ -0,0 +1,12 @@
+class population():
+    def __init__(self):
+        ''' initialize population based on starting resources '''
+        return 
+        
+   def Update(self):
+       return 
+       
+   def Run(self):
+       self.Update()
+
+       
