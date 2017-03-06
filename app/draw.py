fffd8a27fc78ea076fd798060b35291d4b00a57c
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 22:07:52 -0500

    Update draw.py

--- app/draw.py
+++ app/draw.py
@@ -2,9 +2,13 @@ import pygame
 
 
 class draw():
+    
+    def DrawAGridBasedOnWidthAndHeightInBoxes(self, width, height):
+        return 
 
     def FromDrawModuleDrawGrid(self, width, height):
         # draw a grid based on widgth and height
+        self.DrawAGridBasedOnWidthAndHeightInBoxes(width, height)
         return
     
     def Run(self):
