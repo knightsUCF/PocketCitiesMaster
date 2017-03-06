5ad43536f61a4f84b662674ed879233b8c3f324b
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 22:35:10 -0500

    Create perk.py

---
+++ app/perk.py
@@ -0,0 +1,10 @@
+class perk():
+    def __init__(self, ID):
+        self.ID = ID
+        
+    def PerkTypes():
+        self.VIP
+        self.UpstandingCitizen
+        self.Monopoly 
+        self.Bonus
+        self.LevelUp
