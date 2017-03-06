a28cfb72d30421d5c01a8e470f576257ae38b35a
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 20:09:23 -0500

    Create character.py

---
+++ app/character.py
@@ -0,0 +1,10 @@
+class character():
+    def __init__(self):
+        ''' initialize character state data '''
+        return 
+        
+    def Update(self):
+        return 
+        
+    def Run(self):
+        return 
