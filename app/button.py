515a64d2e56c87226206a1983bc6461a7347a7e0
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 23:03:37 -0500

    Update button.py

--- app/button.py
+++ app/button.py
@@ -1,1 +1,33 @@
-
+class button():
+    def __init__(self, CurrentButtonPress):
+        self.CurrentButtonPress = CurrentButtonPress
+        
+    def OnPressUpKey(self):
+        return
+    
+    def OnPressDownKey(self):
+        return
+    
+    def OnPressRightKey(self):
+        return
+    
+    def OnPressLeftKey(self):
+        return
+      
+    def OnPressQKey(self):
+        return
+      
+    def OnPressSpacebar(self):
+        return
+    
+    def OnPressEnter(self):
+        return
+      
+    def OnPressAlt(self):
+        return
+      
+    def OnPressCtrl(self):
+        return 
+      
+    
+      
