493c1c87990705b295f8c650bec9bbb27f8a37a1
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 23:44:49 -0500

    Update ai.py

--- app/ai.py
+++ app/ai.py
@@ -2,16 +2,24 @@ class aiperson():
     def __init__(self, ID):
         self.ID = id
         
-    def GenerateNewIndividual(self, ID):
+    def GenerateNewPerson(self, ID):
         self.ID = ID
         return
     
-    def Name():
+    def NamePerson(self, ID):
         self.NameRandomAIPerson = { 1 : "Bob", 2 : "Jane", 3 : "Brittney", 4 : "Alexa" }
         return
     
-    def GiveFreeWill():
+    def GiveFreeWillToPerson(self, ID):
         return
+    
+    def GiveProfessionToPerson(self, ID):
+        return
+    
+    def GenerateStatsOnPerson(self, ID):
+        return
+    
+    
 
     
     
