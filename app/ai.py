a54dc021c7f6336e60c20b613bc248db502e5e77
peterknights1 <peterk@knights.ucf.edu>
2017-03-05 23:34:25 -0500

    Create ai.py

---
+++ app/ai.py
@@ -0,0 +1,14 @@
+class ai():
+    def __init__(self, ID):
+        self.ID = id
+        
+    def GenerateNewAIIndividual():
+        return
+        
+    def GenerateNewAICluster():
+        return 
+        
+    def AnalyzeProbabilityCloud():
+        return 
+        
+        
