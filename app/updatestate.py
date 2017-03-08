d3362d0ecbd474c68924dc7dc4c3a6219f7e3192
peterknights1 <peterk@knights.ucf.edu>
2017-03-07 19:33:42 -0500

    Create updatestate.py

---
+++ app/updatestate.py
@@ -0,0 +1,10 @@
+ # Here we are going to import state, where we actually keep all the data
+ import state
+ ObjectState = state.state()
+ 
+ # to get values we are going to get them through methods 
+ 
+ 
+# to get input and use to update state
+import processinputintogamelogic # this contains the button class, also gets input through linking the button class
+ObjectProcessInputInto
